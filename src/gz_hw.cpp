ab28acdf865b6f44452c3ba7c8d5b63da696b4aa
Kenji Brameld <kenjibrameld@gmail.com>
2022-09-20 13:34:58 +0900

    change convert_ign_to_ros to convert_gz_to_ros
    Signed-off-by: Kenji Brameld <kenjibrameld@gmail.com>

--- src/gz_hw.cpp
+++ src/gz_hw.cpp
@@ -51,7 +51,7 @@ public:
   void jointStateCallback(const ignition::msgs::Model & ignMsg)
   {
     sensor_msgs::msg::JointState jointState;
-    ros_gz_bridge::convert_ign_to_ros(ignMsg, jointState);
+    ros_gz_bridge::convert_gz_to_ros(ignMsg, jointState);
     for (auto & joint : joints) {
       auto it = find(jointState.name.begin(), jointState.name.end(), joint.name);
       if (it != jointState.name.end()) {
